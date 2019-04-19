1dfe75b8a596645f70026749c3651257dba95ded
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-04-19 10:24:26 +0530

    initial commit

---
+++ first-app/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { details: [] };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    let name = data.get("firstname");
+    event.target.reset();
+    const headers = { "content-type": "application/json" };
+    const body = JSON.stringify({ name });
+    fetch("/add_name", { method: "POST", headers, body })
+      .then(res => res.json())
+      .then(jsonData => this.setState({ details: jsonData.details }));
+  }
+
+  showNames(details) {
+    return details.map(detail => (
+      <div>
+        {" "}
+        {detail.name} launched missile at {detail.time}{" "}
+      </div>
+    ));
+  }
+
+  componentDidMount() {
+    fetch("/get_details")
+      .then(res => res.json())
+      .then(jsonData => {
+        console.log(jsonData);
+        this.setState({ details: jsonData.details });
+      });
+  }
+
+  render() {
+    return (
+      <div className="launching-platform">
+        <div className="launched-missile-details">
+          Missile Launch Info :
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <label>
+              First name:
+              <input type="text" name="firstname" required />
+            </label>
+
+            <input className="launch-button" type="submit" value="Launch" />
+          </form>
+        </div>
+        <div>{this.showNames(this.state.details)} </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/first-app/build"));
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "naruto10",
+  database: "launch_missile"
+});
+
+const launchMissile = function(req, res) {
+  const data = req.body.name;
+
+  var person = { name: data };
+  connection.query("INSERT INTO users SET ?", person, function(err, result) {
+    if (err) console.log(err);
+    console.log(result);
+  });
+  res.redirect("/get_details");
+};
+
+const getDetails = function(req, res) {
+  let q = "SELECT count,name,date_format(launched_at,'%D %M %Y ____ %r') AS date from users;";
+  connection.query(q, function(err, result) {
+    if (err) console.log(err);
+    let details = result.map(detail => {
+      return { name: detail.name, time: detail.date };
+    });
+    res.send({ details });
+  });
+};
+
+app.post("/add_name", launchMissile);
+app.get("/get_details", getDetails);
+app.listen(PORT, () => console.log("listening at port", PORT));
